916fa44f9cb54c9bfc2945f3b771f8862069626c
diyor-dev <behruzsadullayev08@gmail.com>
2024-06-14 19:57:15 +0300

    nmadr ol

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,47 @@ export default function App() {
   return (
     <div>
       <h1>lorem hello</h1>
-      <h2>diyor loh</h2>
-      <h3>ozing loh soska</h3>
+     <div className="">
+      <div>
+        Realco
+      </div>
+      <div>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              
+            
+              </div>
+              <div>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              
+            
+              </div>
+              <div>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              <p>Pages</p>
+              
+            
+              </div>
+     </div>
     </div>
   );
 }
